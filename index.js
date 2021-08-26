6174e71c01546a9fb9eea75813eb3d675371b28a
veemelia <v.mveenaikar@gmail.com>
2021-08-26 17:05:18 +0800

    Finished working with API

--- index.js
+++ index.js
@@ -5,3 +5,20 @@
 // -- What are the required parameters?
 // -- What optional parameters do we want to add?
 // - What HTTP method are we using?
+
+let url = `https://restcountries.eu/rest/v2/name/Australia`;
+
+function waitForData(res) {
+  return res.json();
+}
+
+function handleData(data) {
+  let pop = data[0].population;
+  console.log(`The population of Australia is ${pop}.`);
+}
+
+fetch(url, {
+  method: "GET",
+})
+  .then(waitForData)
+  .then(handleData);
