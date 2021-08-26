80b71b30e8dee86bfc33a9953b5bf0e1526960bb
veemelia <v.mveenaikar@gmail.com>
2021-08-26 16:33:58 +0800

    Added HTML and JS

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("This woeked");
+// Workng with APIs
+// We need to answer the following questions
+// - What is the endpoint (URL)?
+// - What query string/parameters do we need to provide?
+// -- What are the required parameters?
+// -- What optional parameters do we want to add?
+// - What HTTP method are we using?
